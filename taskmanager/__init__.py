d697a1f18a53819b434b600677efcc88bc7e6a1c
hashim222 <aslamhashim19@gmail.com>
2022-07-20 19:03:56 +0000

    imported few files and database models has been updated

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -1,13 +1,15 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
 if os.path.exists("env.py"):
     import env  # noqa
 
+
 app = Flask(__name__)
-app.config["SECRET_KEY"]
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
+# app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://DB_URL"
+
 
 db = SQLAlchemy(app)
 
